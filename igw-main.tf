68f1d104fbb99abba54a3b8dfe3fec5b931fd919
GiorgioBrembati <giorgio.brembati@gmail.com>
2022-08-28 15:41:08 +0200

    updating to arm template to aug-22

--- igw-main.tf
+++ igw-main.tf
@@ -16,44 +16,20 @@ resource "azurerm_resource_group_template_deployment" "template-deployment-appse
   resource_group_name = azurerm_resource_group.rg-appsec-vmss.name
   deployment_mode     = "Complete"
 
-  template_content    = file("files/appsec-vmss-template.json")
+  template_content    = file("files/appsec-vmss-template-aug-22.json")
   parameters_content  = <<PARAMETERS
   {
     "location": {
         "value": "${azurerm_resource_group.rg-appsec-vmss.location}"
     },
-    "vmName": {
-        "value": "${var.appsec-name}"
-    },
-    "inboundSources": {
-        "value": "0.0.0.0/0"
-    },
     "authenticationType": {
         "value": "password"
-    },
+        },
     "adminPassword": {
         "value": "${var.admin-pwd}"
     },
-    "sshPublicKey": {
-        "value": ""
-    },
-    "waapAgentToken": {
-        "value": "${var.infinity-token}"
-    },
-    "waapAgentFog": {
-        "value": ""
-    },
-    "adminEmail": {
-        "value": ""
-    },
-    "availabilityZonesNum": {
-        "value": 2
-    },
-    "vmSize": {
-        "value": "${var.appsec-size}"
-    },
-    "diskType": {
-        "value": "Standard_LRS"
+    "vmName": {
+        "value": "${var.appsec-name}"
     },
     "instanceCount": {
         "value": "${var.appsec-vmss-min}"
@@ -61,41 +37,26 @@ resource "azurerm_resource_group_template_deployment" "template-deployment-appse
     "maxInstanceCount": {
         "value": "${var.appsec-vmss-max}"
     },
-    "instanceLevelPublicIP": {
-        "value": "yes"
-    },
-    "elbResourceId": {
-        "value": ""
-    },
-    "elbTargetBEAddressPoolName": {
-        "value": ""
-    },
     "deploymentMode": {
         "value": "ELBOnly"
+    },
+    "instanceLevelPublicIP": {
+        "value": "yes"
     },
     "appLoadDistribution": {
         "value": "SourceIP"
     },
-    "ilbResourceId": {
-        "value": ""
+    "availabilityZonesNum": {
+        "value": 2
     },
-    "ilbTargetBEAddressPoolName": {
-        "value": ""
-    },
-    "ilbLoadDistribution": {
-        "value": "Default"
+    "vmSize": {
+        "value": "${var.appsec-size}"
     },
     "bootstrapScript": {
         "value": ""
     },
     "sourceImageVhdUri": {
         "value": "noCustomUri"
-    },
-    "vnetNewOrExisting": {
-        "value": "existing"
-    },
-    "virtualNetworkExistingRGName": {
-        "value": "${azurerm_virtual_network.vnet-north.resource_group_name}"
     },
     "virtualNetworkName": {
         "value": "${azurerm_virtual_network.vnet-north.name}"
@@ -104,6 +65,12 @@ resource "azurerm_resource_group_template_deployment" "template-deployment-appse
         "value": [
             "${azurerm_virtual_network.vnet-north.address_space[0]}"
         ]
+    },
+    "vnetNewOrExisting": {
+        "value": "existing"
+    },
+    "virtualNetworkExistingRGName": {
+        "value": "${azurerm_virtual_network.vnet-north.resource_group_name}"
     },
     "subnet1Name": {
         "value": "${azurerm_subnet.net-north-frontend.name}"
@@ -117,15 +84,16 @@ resource "azurerm_resource_group_template_deployment" "template-deployment-appse
     "subnet2Prefix": {
         "value": "${azurerm_subnet.net-north-backend.address_prefixes[0]}"
     },
-    "subnet2StartAddress": {
-        "value": "10.10.1.4"
+    "inboundSources": {
+        "value": "0.0.0.0/0"
     },
-    "check_PointTags": {
-        "value": {
-            "provider": "30DE18BC-F9F6-4F22-9D30-54B8E74CFD5F"
-        }
+    "waapAgentToken": {
+        "value": "${var.infinity-token}"
     },
-    "sicKey": {
+    "waapAgentFog": {
+        "value": ""
+    },
+    "adminEmail": {
         "value": ""
     },
     "chooseVault": {
@@ -139,51 +107,6 @@ resource "azurerm_resource_group_template_deployment" "template-deployment-appse
     },
     "numberOfCerts": {
         "value": 0
-    },
-    "firstCertificate": {
-        "value": ""
-    },
-    "firstCertDescription": {
-        "value": ""
-    },
-    "firstCertPassword": {
-        "value": ""
-    },
-    "secondCertificate": {
-        "value": ""
-    },
-    "secondCertDescription": {
-        "value": ""
-    },
-    "secondCertPassword": {
-        "value": ""
-    },
-    "thirdCertificate": {
-        "value": ""
-    },
-    "thirdCertDescription": {
-        "value": ""
-    },
-    "thirdCertPassword": {
-        "value": ""
-    },
-    "fourthCertificate": {
-        "value": ""
-    },
-    "fourthCertDescription": {
-        "value": ""
-    },
-    "fourthCertPassword": {
-        "value": ""
-    },
-    "fifthCertificate": {
-        "value": ""
-    },
-    "fifthCertDescription": {
-        "value": ""
-    },
-    "fifthCertPassword": {
-        "value": ""
     }
   }
   PARAMETERS 
